55551447326eba1f440faa2f1427a582082d68c0
Juliana Felix <julianafelix.dev@gmail.com>
2022-05-11 02:09:31 -0300

    Ajustes de erro

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -11,19 +11,27 @@ routes.post('/feedbacks', async (req, res) => {
 
   const { type, comment, screenshot } = req.body;
 
-  const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
-  const nodemailerMailAdapter = new NodemailerMailAdapter()
+  try {
 
-  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-    prismaFeedbacksRepository, 
-    nodemailerMailAdapter
-  )
+  
+    const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+    const nodemailerMailAdapter = new NodemailerMailAdapter()
 
-  await submitFeedbackUseCase.execute({
-    type,
-    comment,
-    screenshot,
-  })
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+      prismaFeedbacksRepository, 
+      nodemailerMailAdapter
+    )
 
-  return res.status(201).send();
-})
+    await submitFeedbackUseCase.execute({
+      type,
+      comment,
+      screenshot,
+    });
+
+    return res.status(201).send();
+  } catch (err){
+    console.error(err);
+
+    return res.status(201).send();
+  }
+});
